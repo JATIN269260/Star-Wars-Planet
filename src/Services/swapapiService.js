48b874125c83a774a1e571cd1350e997a910f96c
Jatin Garg <71146552+JATIN269260@users.noreply.github.com>
2024-02-23 22:01:18 +0530

    Added css for pagination

--- src/Services/swapapiService.js
+++ src/Services/swapapiService.js
@@ -1,22 +1,25 @@
-// services/swapiService.js
-import axios from 'axios';
+import axios from "axios";
 
 const swapiService = {
   fetchPlanets: async (page = 1) => {
     try {
-      const response = await axios.get(`https://swapi.dev/api/planets/?format=json&page=${page}`);
+      const response = await axios.get(
+        `https://swapi.dev/api/planets/?format=json&page=${page}`
+      );
       return response.data;
     } catch (error) {
-      throw new Error('Error fetching planets:', error);
+      throw new Error("Error fetching planets:", error);
     }
   },
   fetchResidents: async (residentUrls) => {
-    const residentRequests = residentUrls.map((residentUrl) => axios.get(residentUrl));
+    const residentRequests = residentUrls.map((residentUrl) =>
+      axios.get(residentUrl)
+    );
     try {
       const residentResponses = await Promise.all(residentRequests);
       return residentResponses.map((response) => response.data);
     } catch (error) {
-      throw new Error('Error fetching residents:', error);
+      throw new Error("Error fetching residents:", error);
     }
   },
 };
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,29 +1,50 @@
-// components/Pagination.js
-import React from "react";
-import "../Style/Pagination.css"; // Import the CSS file
+import React, { useState, useEffect } from "react";
+import "../Style/Pagination.css";
+import swapiService from "../Services/swapapiService";
 
-const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+const Pagination = ({ onPageChange }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    fetchData(currentPage);
+  }, [currentPage]);
+
+  const fetchData = async (page) => {
+    try {
+      const data = await swapiService.fetchPlanets(page);
+      setPlanets(data.results);
+      setTotalPages(Math.ceil(data.count / 10));
+    } catch (error) {
+      console.error("Error fetching planets:", error);
+    }
+  };
+
   const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
     onPageChange(newPage);
   };
 
   return (
-    <div className="pagination">
+    <div className="pagination-container">
       {currentPage > 1 && (
         <button
+          className="pagination-item"
           onClick={() => handlePageChange(currentPage - 1)}
           disabled={currentPage === 1}
         >
-          Previous
+          <span className="arrow left"></span>
         </button>
       )}
-      <span>{`${currentPage} / ${totalPages}`}</span>
+      <span className="pagination-item selected">{`${currentPage} / ${totalPages}`}</span>
       {currentPage < totalPages && (
         <button
+          className="pagination-item"
           onClick={() => handlePageChange(currentPage + 1)}
           disabled={currentPage === totalPages}
         >
-          Next
+          <span className="arrow right"></span>
         </button>
       )}
     </div>
